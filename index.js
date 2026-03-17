043abea0493cd4d8368a64c4b618bdf3e986a259
hostdeployment-bit <hostdeployment@gmail.com>
2026-03-18 01:53:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import PastebinAPI from 'pastebin-js'
 import path, { dirname } from 'path'
 import pino from 'pino'
 import { fileURLToPath } from 'url'
-let pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+let pastebin = new PastebinAPI('WgRGngwD6YAaVHaoXpvWN1nnUYMXBA3S')
 
 const app = express()
 
